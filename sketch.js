fc0b724b8c6834bfa788f4cd0c072fb218dd7567
dv145 <82046907+dv145@users.noreply.github.com>
2021-08-05 15:17:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,6 @@ let sliderA;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-  ma = atan(cos(QUARTER_PI));
-  maxD = dist(0, 0, 200, 200);
   ambientLight(230);
   
   //frameRate(30)
@@ -19,7 +17,7 @@ function draw() {
   
   background(0); 
   ortho(-400, 400, 400, -400, 0, 1000);
-    rotateX(ma);
+  rotateX(atan(cos(QUARTER_PI)));
   rotateY(-QUARTER_PI);
 
 
@@ -28,13 +26,11 @@ function draw() {
       
       push();
       
-      let d = dist(x, z, width / 2, height / 2);
+      let dtheta = map(dist(x, z, width / 2, height / 2), 0, dist(0, 0, 200, 200), -PI, PI);
       
-      let offset = map(d, 0, maxD, -PI, PI);
+      let theta = angle + dtheta;
       
-      let a = angle + offset;
-      
-      let h = (map(sin(a), -1, 1, 60, 300));
+      let h = (map(sin(theta), -1, 1, 60, 300));
       
       translate(x - width / 2, 0, z - height / 2);
       normalMaterial();
